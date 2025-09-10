038a941dafc6ea7dfdad82bad0f59074eb4bbd45
Fredrik Jonsson <FredrikAndreJonson@hotmail.com>
2025-09-10 19:57:27 +0200

    Add IBookingRepository interface for booking operations
    Introduced a new `IBookingRepository` interface in the
    `TooliRent.Domain.Interfaces` namespace to manage booking
    operations. The interface includes methods for adding,
    retrieving, and checking bookings, as well as saving changes.
    Added necessary `using` directives to support the interface.

---
+++ TooliRent.Domain/Interfaces/IBookingRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TooliRent.Domain.Entities;
+
+namespace TooliRent.Domain.Interfaces
+{
+    public interface IBookingRepository
+    {
+        Task AddAsync(Booking booking, CancellationToken cancellationToken);
+        Task<Booking?> GetByIdAsync(int id, CancellationToken cancellationToken);
+        Task<Booking?> GetByIdForMemberAsync(int id, string memberId, CancellationToken cancellationToken);
+        Task<List<Booking>> GetForMemberAsync(string memberId, CancellationToken cancellationToken);
+        Task<bool> AnyOverlappingAsync(IEnumerable<int> toolIds, DateTime startDate, DateTime endDate, CancellationToken cancellationToken);
+        Task SaveChangesAsyng(CancellationToken cancellationToken);
+    }
+}
